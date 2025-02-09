aaff84eddbb219f7767d8d26ed442a5be515cf63
tissueMO <ts32worker@gmail.com>
2025-02-09 13:44:20 +0900

    フォローアドオンを廃止

--- extensions/discord/addon/index.js
+++ extensions/discord/addon/index.js
@@ -1,13 +1,11 @@
 const Addon = require('./Addon');
 const HookAddon = require('./HookAddon');
-const FollowAddon = require('./FollowAddon');
 const ShuffleAddon = require('./ShuffleAddon');
 const RecordAddon = require('./RecordAddon');
 
 module.exports = {
   Addon,
   HookAddon,
-  FollowAddon,
   ShuffleAddon,
   RecordAddon,
 };
